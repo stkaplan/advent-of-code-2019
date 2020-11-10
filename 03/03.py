d625da331fd90985c553c833071363df73be3f33
Sam Kaplan <stkaplan@gmail.com>
2020-11-10 12:17:22 -0500

    Day 3 part 2

--- 03/03.py
+++ 03/03.py
@@ -13,10 +13,12 @@ def parse_line(line):
     return [Step(x[0], int(x[1:])) for x in line.split(',')]
 
 def trace_path(path):
-    points = set()
+    points = {}
+    steps = 0
     position = Position(0, 0)
     for step in path:
         for _ in range(step.distance):
+            steps += 1
             if step.direction == 'U':
                 position = Position(position.x, position.y + 1)
             elif step.direction == 'D':
@@ -26,14 +28,15 @@ def trace_path(path):
             elif step.direction == 'R':
                 position = Position(position.x + 1, position.y)
 
-            points.add(position)
+            if position not in points:
+                points[position] = steps
 
     return points
 
 def find_intersections(path1, path2):
     points1 = trace_path(path1)
     points2 = trace_path(path2)
-    return points1.intersection(points2)
+    return {x: points1[x] + points2[x] for x in points1 if x in points2}
 
 def manhattan_distance(p1, p2):
     return abs(p1.x - p2.x) + abs(p1.y - p2.y)
@@ -43,9 +46,12 @@ def find_smallest_distance(points):
 
 def part1(lines):
     paths = parse_input(lines)
-    intersections = trace_path(paths[0]).intersection(trace_path(paths[1]))
-    return find_smallest_distance(intersections)
+    return find_smallest_distance(find_intersections(paths[0], paths[1]))
         
+def part2(lines):
+    paths = parse_input(lines)
+    return min(find_intersections(paths[0], paths[1]).values())
+
 class RunProgramTest(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1([
@@ -57,8 +63,19 @@ class RunProgramTest(unittest.TestCase):
             'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
         ]), 135)
 
+    def test_part2(self):
+        self.assertEqual(part2([
+            'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+            'U62,R66,U55,R34,D71,R55,D58,R83',
+        ]), 610)
+        self.assertEqual(part2([
+            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+        ]), 410)
+
 if __name__ == '__main__':
     unittest.main(exit=False)
 
     with open('input.txt') as f:
-        print(part1(f.readlines()))
+        #print(part1(f.readlines()))
+        print(part2(f.readlines()))
