ca2d8970ce1c716941809266b42a65df72cc81e6
Sam Kaplan <stkaplan@gmail.com>
2020-11-16 19:22:56 -0500

    Day 10 part 2

--- 10/10.py
+++ 10/10.py
@@ -17,16 +17,52 @@ def read_file(filename):
     return asteroids
 
 def get_angle(base, asteroid):
-    x = base.x - asteroid.x
-    y = base.y - asteroid.y
-    return math.atan2(x, y)
+    y = base.y - asteroid.y # +y is towards y = 0
+    x = asteroid.x - base.x # +x is towards right
+    return math.atan2(y, x)
+
+def normalize_angle(angle):
+    # Reverse rotation to go clockwise.
+    angle *= -1
+    # Shift so min is pointing up.
+    angle += math.pi / 2
+    # Push back to interval [0, 2pi]
+    angle = angle % (2 * math.pi)
+    return angle
+
+def get_distance(base, asteroid):
+    return abs(base.x - asteroid.x) + abs(base.y - asteroid.y)
+
+def get_asteroid_angles(base, asteroids):
+    angles = {}
+    for asteroid in asteroids:
+        if asteroid == base: continue
+        angle = get_angle(base, asteroid)
+        if angle not in angles:
+            angles[angle] = []
+        angles[angle].append(asteroid)
+    for angle in angles:
+        angles[angle].sort(key=lambda x: get_distance(base, x))
+    return angles
 
 def count_seen_asteroids(base, asteroids):
-    return len(set(get_angle(base, a) for a in asteroids if a != base))
+    return len(get_asteroid_angles(base, asteroids))
 
 def get_best_base(asteroids):
     possible_bases = ((a, count_seen_asteroids(a, asteroids)) for a in asteroids)
     return max(possible_bases, key=lambda x: x[1])
+
+def vaporize_asteroids(base, asteroids):
+    angles = get_asteroid_angles(base, asteroids)
+    vaporized_asteroids = []
+    while angles:
+        for angle in sorted(angles, key=normalize_angle):
+            asteroid = angles[angle].pop(0)
+            vaporized_asteroids.append(asteroid)
+        # Remove angles that have no more asteroids
+        for angle in [a for a in angles if not angles[a]]:
+            del angles[angle]
+    return vaporized_asteroids
 
 class Test(unittest.TestCase):
     def test_count_seen_asteroids(self):
@@ -55,7 +91,28 @@ class Test(unittest.TestCase):
         run_test('test4.txt', ((6,3), 41))
         run_test('test5.txt', ((11,13), 210))
 
+    def test_vaporize_asteroids(self):
+        asteroids = read_file('test5.txt')
+        vaporized_asteroids = vaporize_asteroids(Position(11, 13), asteroids)
+        self.assertEqual(len(vaporized_asteroids), 299)
+        def run_test(num, x, y):
+            self.assertEqual(vaporized_asteroids[num-1], Position(x, y))
+        run_test(1, 11, 12)
+        run_test(2, 12, 1)
+        run_test(3, 12, 2)
+        run_test(10, 12, 8)
+        run_test(20, 16, 0)
+        run_test(50, 16, 9)
+        run_test(100, 10, 16)
+        run_test(199, 9, 6)
+        run_test(200, 8, 2)
+        run_test(201, 10, 9)
+        run_test(299, 11, 1)
+
 if __name__ == '__main__':
     unittest.main(exit=False)
 
-    print(get_best_base(read_file('input.txt')))
+    asteroids = read_file('input.txt')
+    base, _ = get_best_base(asteroids)
+    vaporized_asteroids = vaporize_asteroids(base, asteroids)
+    print(vaporized_asteroids[199])
